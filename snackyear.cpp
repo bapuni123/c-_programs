16d157db42b951e65d45a1c85438299f194c39c8
ashes <asheskb123@gmail.com>
2021-09-30 20:12:36 +0530

    C++ programs

---
+++ snackyear.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t-->0)
+    {
+        int n;
+        cin>>n;
+        if(n==2010 || n==2015 || n==2016 || n==2017 || n==2019)
+        {
+            cout<<"HOSTED"<<endl;
+        }else{
+            cout<<"NOT HOSTED"<<endl;
+        }
+        
+    }
+    return 0;
+}
